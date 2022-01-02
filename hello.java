b3216ba78f2fa9f151d76d8a791480d8321b67d1
Ravi kumar <rk4259011@gmail.com>
2022-01-02 20:57:40 +0530

    hello world

---
+++ hello.java
@@ -0,0 +1,15 @@
+public class helloworld{
+
+public static void main (String args[]){
+System.out.println("Ravi kumar");
+System.out.println("Ravi kumar"+" "+"Ravi kumar");
+System.out.println("ravi"+" "+"Ram"+" "+"Rahim");
+
+
+
+
+
+
+
+}
+}
