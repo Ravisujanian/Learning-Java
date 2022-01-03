fa258797ed7b3ed22fdb519b4f4193fac992906e
Ravi kumar <rk4259011@gmail.com>
2022-01-03 16:31:48 +0530

    literal

---
+++ hello2.java
@@ -0,0 +1,18 @@
+public class helloworld {
+public static void main ( String [] args ){
+byte age = 34;
+int age2  = 56;
+short age3 = 87;
+long agedino =56666666666l;
+char ch = 'a';
+float f1 = 5.6f;
+double d1 = 4.66;
+boolean a = true;
+System.out.println(age);
+System.out.println(age3);
+System.out.println(age2);
+System.out.println(ch);
+System.out.println(a);
+
+}
+}
