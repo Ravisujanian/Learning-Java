e11bc16fa5d851d28b7641f9943c151264bec72f
Ravi kumar <rk4259011@gmail.com>
2022-01-03 11:29:11 +0530

    sum of numbers

---
+++ hello1.java
@@ -0,0 +1,18 @@
+public class helloworld{
+public static void main (String args[]){
+System.out.println("the sum of these number is: ");
+int num1 = 8;
+int num2 = 9;
+int num3 = 3;
+int num4 = 5;
+int num5 = 14;
+int num6 = 15;
+int sum = num1 + num2 + num3 + num4 + num5 + num6;
+System.out.println(sum);
+
+
+
+
+
+}
+}
