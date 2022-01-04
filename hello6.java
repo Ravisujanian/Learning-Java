70e985a9520c596ec5e557793a4c3b318828f82f
Ravi kumar <rk4259011@gmail.com>
2022-01-04 14:07:18 +0530

    write hello ravi have a good day

---
+++ hello6.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class helloworld {
+public static void main (String args[]){ 
+System.out.println("what is your name");
+Scanner s = new Scanner(System.in);
+String name = s.next();
+System.out.println("Hello" +" "+   name  +" " +  "have a good day;");
+}
+}
