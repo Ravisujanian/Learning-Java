4daf0e11e159a77c8ba2270e443833bad7d4ebc7
Ravi kumar <rk4259011@gmail.com>
2022-01-04 19:35:43 +0530

    a number is detected by user wether it is integer

---
+++ hello7.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class helloworld {
+public static void main (String args[]){
+System.out.println("Enter your number");
+Scanner sc = new Scanner(System.in);
+System.out.println(sc.hasNextInt());
+}
+}
