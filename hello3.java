39ef1ecf4a91b3e8c18acbb0ea386f689eaa41e3
Ravi kumar <rk4259011@gmail.com>
2022-01-04 09:46:10 +0530

    adding three numbers

--- hello3.java
+++ hello3.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class helloworld {
 public static void main (String args []){
 System.out.println ("Taking input from the User");
-Scanner sc = new Scanner(System.in);
+Scanner s = new Scanner(System.in);
 System.out.println("Enter number1");
-int a = sc.nextInt();
+int a = s.nextInt();
 System.out.println("Enter number2");
-int b = sc.nextInt();
+int b = s.nextInt();
 int sum = a +b;
 System.out.println("The sum of these numbers is");
 System.out.println(sum);
---
+++ hello4.java
@@ -0,0 +1,9 @@
+public class helloworld {
+public static void main (String args[] ){
+int a = 8;
+int b = 14;
+int c = 45;
+int sum = a+b+c;
+System.out.println(sum);
+}
+}
