0bc72ce3e4fbd230dcd2be2b373d47e973a34143
Ravi kumar <rk4259011@gmail.com>
2022-01-04 10:43:15 +0530

    cgpa of six subject

---
+++ hello5.java
@@ -0,0 +1,12 @@
+public class helloworld {
+public static void main (String args[]){
+float subject1 = 85;
+float subject2 = 56;
+float subject3 = 65;
+float subject4 = 72;
+float subject5 = 82;
+float subject6 = 68;
+float cgpa = (subject1+subject2+subject3+subject4+subject5+subject6)/60;
+System.out.println(cgpa);
+}
+}
